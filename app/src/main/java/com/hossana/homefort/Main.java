a02d63cf83d1db628d8a56e07ceef5f6f6e4ca32
Hossanadev <hossanacc@gmail.com>
2024-05-15 19:27:14 +0100

    fix: bottom nav light mode

--- app/src/main/java/com/hossana/homefort/Main.java
+++ app/src/main/java/com/hossana/homefort/Main.java
@@ -10,10 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hossana.homefort.utils.DarkModeChecker;
 import com.hossana.homefort.utils.GreetingHandler;
 
 public class Main extends AppCompatActivity {
     TextView greeting;
+    BottomNavigationView bottom_navigation_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,12 @@ public class Main extends AppCompatActivity {
         });
 
         greeting = findViewById(R.id.greeting);
+        bottom_navigation_view = findViewById(R.id.bottom_navigation_view);
+
         greeting.setText(GreetingHandler.getGreeting());
+
+        if (!DarkModeChecker.isDarkModeEnabled(getResources().getConfiguration())) {
+            bottom_navigation_view.setBackgroundResource(R.drawable.bottom_nav_bg_light);
+        }
     }
 }
